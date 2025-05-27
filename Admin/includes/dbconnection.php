3636aae02ef468336ed177fb92651f52979eca9d
Izzra <izzrashzleen@gmail.com>
2025-05-28 05:01:31 +0800

    admin port

--- Admin/includes/dbconnection.php
+++ Admin/includes/dbconnection.php
@@ -6,7 +6,7 @@ $dbname = "mypetakom"; // Your database name
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
