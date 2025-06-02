4b4737ac5928a2b39404f2f101e4e249ac9f4860
Izzra <izzrashzleen@gmail.com>
2025-06-02 15:09:04 +0800

    sidebar advisor

--- Event Advisor/includes/advisor-sidebar.php
+++ Event Advisor/includes/advisor-sidebar.php
@@ -74,17 +74,17 @@
                     </a>
                 </li>
                 <li>
-                    <a href="CommitteeEvent.php">
+                    <a href="Module2/CommitteeEvent.php">
                         <i class="fas fa-plus-circle me-2"></i> Committee Event
                     </a>
                 </li>
                 <li>
-                    <a href="admin-add-activity.php">
+                    <a href="Module2/MeritEvent.php">
                         <i class="fas fa-plus-circle me-2"></i> Merit Event
                     </a>
                 </li>
                 <li>
-                    <a href="admin-add-activity.php">
+                    <a href="Module2/QRevent.php">
                         <i class="fas fa-list-alt me-2"></i> QRCode Event
                     </a>
                 </li>
