a8e444ed91a1e489d89aaf318efdf174ba030e23
AthirahSN <athirahnahar5@gmail.com>
2025-06-03 15:03:38 +0800

    buang port

--- Event Advisor/Module3/db.php
+++ Event Advisor/Module3/db.php
@@ -3,12 +3,12 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "mypetakom_db";
-$port = "3307";
 
 
 
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
+
+$conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
